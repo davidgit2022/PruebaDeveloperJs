3b41e950dc3ffe24183f170cf98df8b5e1b0ab1c
Deivy Potosi <deibypotosi535@gmail.com>
2023-04-20 10:43:37 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+// Configuración de la aplicación Express
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// Ruta para la página de inicio
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// Ruta para manejar la creación de nuevos usuarios
+app.post('/api/users', (req, res) => {
+    const name = req.body.name;
+    const id = req.body.id;
+    const phone = req.body.phone;
+
+    // Validación de datos del usuario
+    if (!name || !id || !phone) {
+        return res.status(400).json({ error: 'Faltan campos requeridos' });
+    }
+
+    // Carga de los usuarios existentes
+    let users = [];
+    try {
+        users = JSON.parse(fs.readFileSync(__dirname + '/data/users.json', 'utf8'));
+    } catch (error) { }
+
+    // Comprobación de que no exista ya un usuario con el mismo ID
+    const existingUser = users.find((user) => user.id === id);
+    if (existingUser) {
+        return res.status(409).json({ error: 'Ya existe un usuario con este ID' });
+    }
+
+    // Agregado del nuevo usuario a la lista
+    users.push({ name, id, phone });
+    fs.writeFileSync(__dirname + '/data/users.json', JSON.stringify(users));
+
+    res.json({ success: true });
+});
+
+// Ruta para recuperar la lista de usuarios
+app.get('/api/users', (req, res) => {
+    let users = [];
+    try {
+        users = JSON.parse(fs.readFileSync(__dirname + '/data/users.json', 'utf8'));
+    } catch (error) { }
+
+    res.json(users);
+});
+
+// Inicio del servidor
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
---
+++ public/app.js
@@ -0,0 +1,50 @@
+// Manipulación del DOM para mostrar la lista de usuarios
+function showUserList(users) {
+    const userList = $('#user-list');
+    userList.empty();
+    users.forEach((user) => {
+        const item = $('<li></li>');
+        item.text(`${user.name} (${user.id}) - ${user.phone}`);
+        userList.append(item);
+    });
+}
+
+// Manejo del envío del formulario
+$('#user-form').submit((event) => {
+    event.preventDefault();
+
+    const name = $('#name').val();
+    const id = $('#id').val();
+    const phone = $('#phone').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/users',
+        data: { name, id, phone },
+        success: () => {
+            $('#name').val('');
+            $('#id').val('');
+            $('#phone').val('');
+            loadUserList();
+        },
+        error: (xhr) => {
+            alert(xhr.responseJSON.error);
+        },
+    });
+});
+
+// Carga de la lista de usuarios desde el servidor
+function loadUserList() {
+    $.ajax({
+        type: 'GET',
+        url: '/api/users',
+        success: (users) => {
+            showUserList(users);
+        },
+    });
+}
+
+// Inicialización de la aplicación
+$(document).ready(() => {
+    loadUserList();
+});

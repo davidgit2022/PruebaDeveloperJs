5123e449a4eb7c801532dcc928c6b2895283daed
Deivy Potosi <deibypotosi535@gmail.com>
2023-04-20 14:48:46 -0500

    Sin middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,4 @@
-const express = require('express');
-const apiRouter = require('./server');
-const app = express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
-
-// Configuración de la aplicación Express
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static('public'));
-
-// Ruta para la página de inicio
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-apiRouter(app);
+const app = require('./app')
 
 // Inicio del servidor
 const port = process.env.PORT || 3000;
